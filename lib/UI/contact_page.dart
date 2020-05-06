01717f7f59be4b8b2a8e6b395fadc7be398dd86f
Guilherme Azevedo <gui-azesantos@hotmail.com>
2020-05-05 21:49:50 -0300

    Projeto Finalizado

--- lib/UI/contact_page.dart
+++ lib/UI/contact_page.dart
@@ -1,13 +1,14 @@
+import 'dart:async';
 import 'dart:io';
 
-
-import '../helpers/contact_helper.dart';
+import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
-
+import 'package:image_picker/image_picker.dart';
 
 class ContactPage extends StatefulWidget {
 
   final Contact contact;
+
   ContactPage({this.contact});
 
   @override
@@ -15,6 +16,7 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
@@ -22,69 +24,77 @@ class _ContactPageState extends State<ContactPage> {
   final _nameFocus = FocusNode();
 
   bool _userEdited = false;
+
   Contact _editedContact;
 
   @override
   void initState() {
     super.initState();
 
-    if (widget.contact == null) {
+    if(widget.contact == null){
       _editedContact = Contact();
     } else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
 
       _nameController.text = _editedContact.name;
-      _emailController.text= _editedContact.email;
+      _emailController.text = _editedContact.email;
       _phoneController.text = _editedContact.phone;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.red,
           title: Text(_editedContact.name ?? "Novo Contato"),
           centerTitle: true,
         ),
         floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.save),
-          onPressed: () {
-            if(_editedContact.name.isNotEmpty && _editedContact.name != null){
+          onPressed: (){
+            if(_editedContact.name != null && _editedContact.name.isNotEmpty){
               Navigator.pop(context, _editedContact);
-            }else{
+            } else {
               FocusScope.of(context).requestFocus(_nameFocus);
             }
           },
+          child: Icon(Icons.save),
           backgroundColor: Colors.red,
         ),
         body: SingleChildScrollView(
-            padding: EdgeInsets.all(10.0),
-            child: Column(children: <Widget>[
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
               GestureDetector(
-                child: Card(
-                    child: Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Row(
-                          children: <Widget>[
-                            Container(
-                                width: 80.0,
-                                height: 80.0,
-                                decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    image: DecorationImage(
-                                        image: _editedContact.img != null
-                                            ? FileImage(
-                                                File(_editedContact.img))
-                                            : AssetImage("images/person.png"))))
-                          ],
-                        ))),
+                child: Container(
+                  width: 140.0,
+                  height: 140.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: _editedContact.img != null ?
+                        FileImage(File(_editedContact.img)) :
+                        AssetImage("images/person.png"),
+                        fit: BoxFit.cover
+                    ),
+                  ),
+                ),
+                onTap: (){
+                  ImagePicker.pickImage(source: ImageSource.camera).then((file){
+                    if(file == null) return;
+                    setState(() {
+                      _editedContact.img = file.path;
+                    });
+                  });
+                },
               ),
               TextField(
-              controller: _nameController,
-              focusNode: _nameFocus,
+                controller: _nameController,
+                focusNode: _nameFocus,
                 decoration: InputDecoration(labelText: "Nome"),
-                onChanged: (text) {
+                onChanged: (text){
                   _userEdited = true;
                   setState(() {
                     _editedContact.name = text;
@@ -94,7 +104,7 @@ class _ContactPageState extends State<ContactPage> {
               TextField(
                 controller: _emailController,
                 decoration: InputDecoration(labelText: "Email"),
-                onChanged: (text) {
+                onChanged: (text){
                   _userEdited = true;
                   _editedContact.email = text;
                 },
@@ -103,12 +113,48 @@ class _ContactPageState extends State<ContactPage> {
               TextField(
                 controller: _phoneController,
                 decoration: InputDecoration(labelText: "Phone"),
-                onChanged: (text) {
+                onChanged: (text){
                   _userEdited = true;
                   _editedContact.phone = text;
                 },
                 keyboardType: TextInputType.phone,
-              )
-            ])));
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  Future<bool> _requestPop(){
+    if(_userEdited){
+      showDialog(context: context,
+        builder: (context){
+          return AlertDialog(
+            title: Text("Descartar Alterações?"),
+            content: Text("Se sair as alterações serão perdidas."),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Cancelar"),
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text("Sim"),
+                onPressed: (){
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        }
+      );
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
 }
